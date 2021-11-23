3b9467fc5a9a3d6b836fb5d717375e29c3c71d54
David Torres <davidthekoa@gmail.com>
2021-11-23 18:40:20 -0500

    Adding inputs and fixing bugs

--- Beginner/day_4/tictactoe.py
+++ Beginner/day_4/tictactoe.py
@@ -1,3 +1,19 @@
+# def cordenate(player):
+#     if player == a1:
+#         player = 1
+#     elif player == b1:
+#         player = 2
+#     elif player == c1:
+#         player = 3
+#     elif player == a2:
+#         player = 4
+#     elif player == b2:
+#         player = 5
+#     elif player == c2:
+#         player = 6
+#     return player
+
+
 print('''
                                                                       
   ,d    88              ,d                            ,d                          88
@@ -9,6 +25,10 @@ MM88MMM 88  ,adPPYba, MM88MMM ,adPPYYba,  ,adPPYba, MM88MMM ,adPPYba,   ,adPPYba
   "Y888 88  `"Ybbd8"'   "Y888 `"8bbdP"Y8  `"Ybbd8"'   "Y888 `"YbbdP"'   `"Ybbd8"' 88
   
 ''')
+
+print('Hi there!\nI\'m Machine and we are about to play TICTACTOE together! :D')
+user_name = (input('So... tell me what your nickname is: '))
+print(f'Amazing "{user_name}"! Let\'s start to play!\n')
 
 row1 = list('   a     b     c')
 row2 = list('      |     |     ')
@@ -29,19 +49,21 @@ rowg = ''
 rowh = ''
 
 
-tictactoe = [row1, row2, row3, row4, row5, row6, row7, row8]
-print(f'{rowa.join(row1)}\n{rowb.join(row2)}\n{rowc.join(row3)}\n{rowd.join(row4)}\n{rowe.join(row5)}\n{rowf.join(row6)}\n{rowg.join(row7)}\n{rowh.join(row8)}')
+# tictactoe = [row1, row2, row3, row4, row5, row6, row7, row8]
 
 # letter tells me about column 3, 9 or 15
 # number tells me about row 1(row3), 2(row5) or 3(row7)
 
-player = 'b2'
+print('So, in order to mark down the spot you want to select, build your cordenate\nselecting between a, b or c column and between 1, 2 or 3 for row, e.g: b1.\n')
+print(f'{rowa.join(row1)}\n{rowb.join(row2)}\n{rowc.join(row3)}\n{rowd.join(row4)}\n{rowe.join(row5)}\n{rowf.join(row6)}\n{rowg.join(row7)}\n{rowh.join(row8)}\n')
+
+player = input('What is your first cordenate?: ')
+
+
 
 col = player[0]
 row = player[1]
 
-print(col)
-print(row)
 
 if col == 'a':
     col = 3
@@ -57,4 +79,14 @@ elif row == '2':
 else:
     row7[col] = 'O'
 
-print(f'{rowa.join(row1)}\n{rowb.join(row2)}\n{rowc.join(row3)}\n{rowd.join(row4)}\n{rowe.join(row5)}\n{rowf.join(row6)}\n{rowg.join(row7)}\n{rowh.join(row8)}')
+print(f'{rowa.join(row1)}\n{rowb.join(row2)}\n{rowc.join(row3)}\n{rowd.join(row4)}\n{rowe.join(row5)}\n{rowf.join(row6)}\n{rowg.join(row7)}\n{rowh.join(row8)}\n')
+
+print('Well done, no is my turn!\n')
+
+if row5[9] == '-':
+    row5[9] = 'X'
+# else:
+    
+
+# print(f'{rowa.join(row1)}\n{rowb.join(row2)}\n{rowc.join(row3)}\n{rowd.join(row4)}\n{rowe.join(row5)}\n{rowf.join(row6)}\n{rowg.join(row7)}\n{rowh.join(row8)}\n')
+

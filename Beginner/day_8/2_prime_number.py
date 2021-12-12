e245daba8545bdfea3f833f9496bb8ba7ee42738
David Torres <davidthekoa@gmail.com>
2021-12-11 23:05:31 -0500

    Initial commit

---
+++ Beginner/day_8/2_prime_number.py
@@ -0,0 +1,27 @@
+
+
+def prime_checker(number):
+  prime = True
+  if number <= 1:
+    prime = False
+  for i in range(2, number - 1):
+    if number > 1:
+      if number % i == 0:
+        prime = False
+        break
+  
+  if prime == True:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+
+    
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = float(input("Check this number: "))
+if n % 1 != 0:
+  print("It's not a prime number.")
+else:
+  # int(n)
+  prime_checker(number=int(n))

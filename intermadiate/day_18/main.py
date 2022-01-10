3160da3e77c1e29a271cb948d5eaec85bce65310
DavidTG7 <davidthekoa@gmail.com>
2022-01-09 22:42:39 -0500

    Creting a drawing with Turtle module

---
+++ intermadiate/day_18/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+import random
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("arrow")
+timmy_the_turtle.color("#285078")
+timmy_the_turtle.forward(100)
+
+screen = Screen()
+screen.exitonclick()

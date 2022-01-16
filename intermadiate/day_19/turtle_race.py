c88e720cf49f771951cba7268d09541de5c0b055
DavidTG7 <davidthekoa@gmail.com>
2022-01-15 23:54:55 -0500

    Initial commit

---
+++ intermadiate/day_19/turtle_race.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win tha race? Enter a color: ")
+colors = ["orange", "blue", "green", "brown", "gold", "red"]
+y_positions = [-70, ]
+
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
+screen.exitonclick()
